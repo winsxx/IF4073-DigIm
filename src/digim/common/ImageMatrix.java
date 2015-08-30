d682271b09c0e9b5ad15c1e824853236705f469c
opelhoward <opelhoward@yahoo.com>
2015-08-30 10:45:33 +0700

    Basic implementation of ImageMatrix class

--- src/digim/common/ImageMatrix.java
+++ src/digim/common/ImageMatrix.java
@@ -3,18 +3,20 @@ package digim.common;
 import digim.common.color.Color;
 
 public class ImageMatrix<ColorType extends Color> {
-    private ColorType[][] matrix;
-    
-    public void ColorMetrix(int width, int height){
-        
+
+    private final ColorType[][] matrix;
+
+    @SuppressWarnings("unchecked")
+    public ImageMatrix(int width, int height) {
+        matrix = (ColorType[][]) new Object[height][width];
     }
-    
-    public void setColor(int row, int col, int ColorType){
-        
+
+    public void setColor(int row, int col, ColorType colorType) {
+        matrix[row][col] = colorType;
     }
-    
-    public ColorType getColor(int row, int col){
-        return null;
+
+    public ColorType getColor(int row, int col) {
+        return matrix[row][col];
     }
     
 }
