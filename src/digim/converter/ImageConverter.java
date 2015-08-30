580b005fd4fb26c0e4557abf46abb77a50702f18
opelhoward <opelhoward@yahoo.com>
2015-08-30 10:47:46 +0700

    Basic implementation of ImageConverter class

--- src/digim/converter/ImageConverter.java
+++ src/digim/converter/ImageConverter.java
@@ -1,14 +1,32 @@
 package digim.converter;
 
 import digim.common.ImageMatrix;
+import digim.common.color.RgbaColor;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class ImageConverter {
-    public void ImageConberter(){
+
+    BufferedImage image;
+
+    ImageConverter(File file) throws IOException {
+        image = ImageIO.read(file);
+    }
+
+    public ImageMatrix toImageMatrix(Boolean withAlpha) {
+        ImageMatrix imageMatrix;
+        imageMatrix = new ImageMatrix<>(image.getWidth(), image.getHeight());
         
+        for (int i = image.getMinY(); i < image.getHeight(); ++i) {
+            for (int j = image.getMinX(); j < image.getWidth(); ++j) {
+                RgbaColor color = new RgbaColor(image.getRGB(j, i));
+                imageMatrix.setColor(i, j, color);
+            }
+        }
+        
+        return imageMatrix;
     }
     
-    public ImageMatrix toImageMatrix(BufferedImage bufferedImage){
-        return null;
-    }
 }
